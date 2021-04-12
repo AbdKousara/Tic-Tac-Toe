68b987ecb07f0fea82385eee038533811512f7f0
AbdoKousara <abdelkader.kawsara@gmail.com>
2021-04-12 10:56:09 +0200

    improve game logic

--- js/Game.js
+++ js/Game.js
@@ -9,8 +9,35 @@ export default class Game{
        }
 
        makeMove(i){
+              if(this.board[i]){
+                     return;
+              }
               this.board[i] = this.turn;
               this.nextTurn();
 
        }
+
+       findWinningComintion(){
+              const winningCombinations = [ 
+                     [0, 1, 2],
+                     [3, 4, 5],
+                     [6, 7, 8],
+                     [0, 3, 6],
+                     [1, 4, 7],
+                     [2, 5, 8],
+                     [0, 4, 8],
+                     [2, 4, 6]
+              ]
+
+              for(const combinations of winningCombinations){
+                     const  [a, b, c] = combinations;
+                     if(this.board[a] && (this.board[a] === this.board[b] )){
+                            return combinations;
+                     }
+                     return null;
+                     
+              }
+       }
+
+       
 } 
